434bb30f3f16eed3630f5ff7bbfa7473eedaa999
iamaj <iamajavagod@gmail.com>
2025-06-29 00:29:34 +0200

    Delete trash

--- src/main/kotlin/com/lvsmsmch/aichat/_trash/PasswordResetTokenRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.lvsmsmch.aichat._trash
-
-import org.litote.kmongo.coroutine.CoroutineCollection
-
-class PasswordResetTokenRepository(
-    collection: CoroutineCollection<VerificationTokenDbo>
-): VerificationTokenRepository(collection)
--- src/main/kotlin/com/lvsmsmch/aichat/_trash/db/_trash/code_trackings/CodeTrackingsForVerifyEmailRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.lvsmsmch.aichat._trash.db._trash.code_trackings
-
-import org.litote.kmongo.coroutine.CoroutineCollection
-
-class CodeTrackingsForVerifyEmailRepository(
-    collection: CoroutineCollection<CodeTrackingDbo>
-): CodeTrackingsBaseRepository(collection)
